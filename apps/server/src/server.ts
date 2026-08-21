0f3c03e3e2602e525b2963f5ac9a9dc5e929e02b
tejaskale99 <tejaskale1556@gmail.com>
2026-08-21 23:59:18 +0530

    style: format code for consistency in server.ts

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config';
-import app from './app.js';
+import "dotenv/config";
+import app from "./app.js";
 
 const port = Number(process.env.PORT);
 
 const startServer = async () => {
-    app.listen(port, () => {
-        console.log(`🚀 Server is running on port ${port}`);
-    })
-}
+  app.listen(port, () => {
+    console.log(`🚀 Server is running on port ${port}`);
+  });
+};
 
-startServer().catch((err)=>{
-    console.error("Failed to start server", err)
-    process.exit(1)
-})
+startServer().catch((err) => {
+  console.error("Failed to start server", err);
+  process.exit(1);
+});
