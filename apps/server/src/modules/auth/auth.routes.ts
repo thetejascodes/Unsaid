31ad74ba2d094b689c824d95c2b4aec843463c07
tejaskale99 <tejaskale1556@gmail.com>
2026-08-23 02:05:14 +0530

    feat(auth): add verification  feature end to end ,route for OTP validation

--- apps/server/src/modules/auth/auth.routes.ts
+++ apps/server/src/modules/auth/auth.routes.ts
@@ -2,8 +2,9 @@ import * as authController from "./auth.controllers.js";
 import { Router } from "express";
 import validate from "../../common/middlewares/vaidate.middleware.js";
 import RequestOtpDto from "./dto/RequestOtpDto.js";
+import VerifyOtpDto from "./dto/VerifyOtpDto.js";
 const router = Router()
 
 router.post('/otp/request',validate(RequestOtpDto),authController.requestOtp);
-
+router.post('/otp/verify',validate(VerifyOtpDto),authController.verifyOtp);
 export default router;
