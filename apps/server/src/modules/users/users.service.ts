c99b178e382ef6562fd060bad7cf2681a16512a5
tejaskale99 <tejaskale1556@gmail.com>
2026-08-23 23:52:17 +0530

    feat(users): implement getMe service to retrieve user details securely

--- apps/server/src/modules/users/users.service.ts
+++ apps/server/src/modules/users/users.service.ts
@@ -1,8 +1,15 @@
 import { db } from "../../common/db/index.js";
 import ApiError from "../../common/utils/api-error.js";
 import { users } from "../auth/auth.schema.js";
+import {eq} from "drizzle-orm";
 
 const getMe = async(userId:string)=>{
+    const [user] = await db.select().from(users).where(eq(users.id,userId))
+    if(!user){
+        throw ApiError.notFound("User not found")
+    }
+    const { phoneHash,banReason,...safeUser } = user;
+    return {user:safeUser};
 }
 
 const updateMe = async(userId:string,updates:any)=>{
