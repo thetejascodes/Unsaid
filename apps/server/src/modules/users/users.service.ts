55905cf48a986135d2ac84e6799e5f11bc15dd9b
tejaskale99 <tejaskale1556@gmail.com>
2026-08-23 23:37:13 +0530

    feat(users): implement getMe and updateMe functions in users service

---
+++ apps/server/src/modules/users/users.service.ts
@@ -0,0 +1,12 @@
+import { db } from "../../common/db/index.js";
+import ApiError from "../../common/utils/api-error.js";
+import { users } from "../auth/auth.schema.js";
+
+const getMe = async(userId:string)=>{
+}
+
+const updateMe = async(userId:string,updates:any)=>{
+
+}
+
+export { getMe, updateMe }
