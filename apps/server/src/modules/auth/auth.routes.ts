a008d1ca0b52c3ce78bea668b34e749f461c8f90
tejaskale99 <tejaskale1556@gmail.com>
2026-08-23 15:05:21 +0530

    feat(auth): add refresh access end to end feature and  token route with validation

--- apps/server/src/modules/auth/auth.routes.ts
+++ apps/server/src/modules/auth/auth.routes.ts
@@ -3,8 +3,10 @@ import { Router } from "express";
 import validate from "../../common/middlewares/vaidate.middleware.js";
 import RequestOtpDto from "./dto/RequestOtpDto.js";
 import VerifyOtpDto from "./dto/VerifyOtpDto.js";
+import RefreshAccessTokenDto from "./dto/RefreshAccessTokenDto.js";
 const router = Router()
 
 router.post('/otp/request',validate(RequestOtpDto),authController.requestOtp);
 router.post('/otp/verify',validate(VerifyOtpDto),authController.verifyOtp);
+router.post('/refresh',validate(RefreshAccessTokenDto),authController.refreshAccessToken)
 export default router;
