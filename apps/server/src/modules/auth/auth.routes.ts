c0c0ca5e5e414c04d91abb80ae986550ec0991e0
tejaskale99 <tejaskale1556@gmail.com>
2026-08-23 19:07:04 +0530

    feat(auth): add isAuthenticated middleware to logout route for enhanced security

--- apps/server/src/modules/auth/auth.routes.ts
+++ apps/server/src/modules/auth/auth.routes.ts
@@ -4,10 +4,11 @@ import validate from "../../common/middlewares/vaidate.middleware.js";
 import RequestOtpDto from "./dto/RequestOtpDto.js";
 import VerifyOtpDto from "./dto/VerifyOtpDto.js";
 import RefreshAccessTokenDto from "./dto/RefreshAccessTokenDto.js";
+import isAuthenticated from "../../common/middlewares/auth.middleware.js";
 const router = Router()
 
 router.post('/otp/request',validate(RequestOtpDto),authController.requestOtp);
 router.post('/otp/verify',validate(VerifyOtpDto),authController.verifyOtp);
 router.post('/refresh',validate(RefreshAccessTokenDto),authController.refreshAccessToken)
-router.post('/logout',validate(RefreshAccessTokenDto),authController.logout)
+router.post('/logout',isAuthenticated,validate(RefreshAccessTokenDto),authController.logout)
 export default router;
