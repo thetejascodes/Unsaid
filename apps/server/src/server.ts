91a274398e872f74075a76a11cd7cedb90970bb1
tejaskale99 <tejaskale1556@gmail.com>
2026-08-28 00:38:41 +0530

    feat(server): register chat handlers during server startup

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -5,6 +5,7 @@ import config from "./common/config/index.js";
 import { attachWebSocketServer } from "./common/ws/server.js";
 import { registerMatchingHandlers } from "./modules/matching/matching.gateway.js";
 import { connectRedis } from "./common/redis/index.js";
+import { registerChatHandlers } from "./modules/chat/chat.gateway.js";
 
 const port = Number(config.port);
 
@@ -13,6 +14,7 @@ const startServer = async () => {
   const httpServer = createServer(app)
   attachWebSocketServer(httpServer);
   registerMatchingHandlers();
+  registerChatHandlers();
   httpServer.listen(port, () => {
     console.log(`🚀 Server is running on port ${port}`);
   });
