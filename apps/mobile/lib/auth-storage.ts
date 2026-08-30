6f0b07ae6ef4f57ba224caf9e61e21886a130f27
tejaskale99 <tejaskale1556@gmail.com>
2026-08-30 13:46:19 +0530

    feat(auth): implement functions to manage refresh token using SecureStore

---
+++ apps/mobile/lib/auth-storage.ts
@@ -0,0 +1,13 @@
+import * as SecureStore from "expo-secure-store";
+
+export const saveRefreshToken = async(token:string)=>{
+    await SecureStore.setItemAsync('refreshToken',token)
+
+}
+export const getRefreshToken = async()=>{
+    await SecureStore.getItemAsync('refreshToken');
+}
+
+export const clearRefreshToken = async()=>{
+    await SecureStore.deleteItemAsync('refreshToken')
+}
