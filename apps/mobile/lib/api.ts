b903d69ade459d34c2ed5e708300734df0f632ed
tejaskale99 <tejaskale1556@gmail.com>
2026-08-30 19:00:59 +0530

    feat(api): implement apiFetch function with token refresh logic

---
+++ apps/mobile/lib/api.ts
@@ -0,0 +1,48 @@
+import {
+  saveRefreshToken,
+  clearRefreshToken,
+  getRefreshToken,
+} from "./auth-storage";
+
+const BASE_URL = process.env.BASE_URL || "http://localhost:8000";
+let currentAccessToken: string = "";
+export const setAccessToken = (token: string) => {
+  return (currentAccessToken = token);
+};
+
+export const apiFetch = async (path: string, options: RequestInit = {}) => {
+  const response = await fetch(BASE_URL + path, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${currentAccessToken}`,
+    },
+  });
+
+  if (response.status === 401) {
+    const refreshToken = await getRefreshToken();
+    if (!refreshToken) {
+      await clearRefreshToken();
+      throw new Error("needs login");
+    }
+    try {
+      const refreshResponse = await fetch(BASE_URL + "/api/auth/refresh", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ refreshToken }),
+      });
+      if (!refreshResponse.ok) {
+        throw new Error("you need to login");
+      }
+      const { data } = await refreshResponse.json();
+      currentAccessToken = data.accessToken;
+      await saveRefreshToken(data.refreshToken);
+      return apiFetch(path, options);
+    } catch (error) {
+      await clearRefreshToken();
+      throw new Error("needs login");
+    }
+  }
+
+  return response;
+};
